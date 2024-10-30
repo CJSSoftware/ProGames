43a7f82ebff2e36452f04890d9f1fe5a99a48923
DarkUnScripted <SuperCarHeaven.NL@gmail.com>
2024-10-30 18:35:07 +0100

    Added bedwars baseplate

--- src/main/java/net/cjsmc/progames/ProGames.java
+++ src/main/java/net/cjsmc/progames/ProGames.java
@@ -1,17 +1,26 @@
 package net.cjsmc.progames;
 
+import net.cjsmc.progames.bedwars.BedWars;
 import net.cjsmc.progames.commandManager.CommandManager;
 import net.cjsmc.progames.commandManager.ExampleCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ProGames extends JavaPlugin {
+
+    private BedWars bedWars;
+
     public void onEnable() {
        //Startup logic
         new CommandManager(this, new ExampleCommand());
+        this.bedWars = new BedWars();
     }
 
     @Override
     public void onDisable() {
         // Shutdown logic
+    }
+
+    public BedWars getBedWars() {
+        return bedWars;
     }
 }
---
+++ src/main/java/net/cjsmc/progames/bedwars/BedWars.java
@@ -0,0 +1,14 @@
+package net.cjsmc.progames.bedwars;
+
+public class BedWars {
+
+    public BedWars() {
+        // Constructor logic
+        registerCommands();
+    }
+
+    private void registerCommands() {
+        // Register commands
+    }
+
+}
